fd7d1a235e345b9e68a772353b96112da97dc782
DuDuncan <tmduncan60@gmail.com>
2015-11-17 18:01:02 +0800

    db init

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+import os
+
 CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 
@@ -7,3 +9,8 @@ OPENID_PROVIDERS = [
     { 'name': 'AOL', 'url': 'http://openid.aol.com/<username>' },
     { 'name': 'Flickr', 'url': 'http://www.flickr.com/<username>' },
     { 'name': 'MyOpenID', 'url': 'https://www.myopenid.com' }]
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URL = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
