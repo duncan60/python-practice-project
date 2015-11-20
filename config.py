c3a129f69a8d2352e71c8bc1f72198e9d89afe18
DuDuncan <tmduncan60@gmail.com>
2015-11-20 16:53:52 +0800

    db create init

--- config.py
+++ config.py
@@ -12,5 +12,5 @@ OPENID_PROVIDERS = [
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
