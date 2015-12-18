e056381f8210a3d320577a4042a296bfbe0e7917
DuDuncan <tmduncan60@gmail.com>
2015-12-18 17:41:51 +0800

    complete paginate step

--- app/models.py
+++ app/models.py
@@ -52,6 +52,7 @@ class User(db.Model):
         return self.followed.filter(followers.c.followed_id == user.id).count() > 0
 
     def followed_posts(self):
+        #return Post.query.order_by(Post.timestamp.desc())
         return Post.query.join(followers, (followers.c.followed_id == Post.user_id)).filter(followers.c.follower_id == self.id).order_by(Post.timestamp.desc())
 
     @staticmethod
@@ -77,4 +78,5 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post %r>' % (self.body)
+
 
--- config.py
+++ config.py
@@ -19,6 +19,7 @@ MAIL_SERVER = 'localhost'
 MAIL_PORT = 25
 MAIL_USERNAME = None
 MAIL_PASSWORD = None
+POSTS_PER_PAGE = 3
 
 # administrator list
 ADMINS = ['you@example.com']
